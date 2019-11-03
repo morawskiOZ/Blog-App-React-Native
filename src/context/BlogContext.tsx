5e5dd1426eb8470d99a848e4630664674bd86c76
Piotr Morawski <piotrmorawski90@gmail.com>
2019-11-03 10:10:26 +0100

    add delete post

--- src/context/BlogContext.tsx
+++ src/context/BlogContext.tsx
@@ -1,36 +1,53 @@
+import { Dispatch } from 'react'
 import createDataContext from './createDataContext'
 
 interface BlogPost {
   title: string
+  id: number
 }
 
 interface BlogContextInterface {
   blogPosts: BlogPost[]
   addBlogPost?: () => void
+  deleteBlogPost: (id: number) => void
 }
 
 export enum BlogPostActions {
-  ADD_BLOG_POST = 'add_blog_post'
+  ADD_BLOG_POST = 'add_blog_post',
+  DELETE_BLOG_POST = 'delete_blog_post'
 }
 
-const blogReducer = (state: BlogPost[], action: any) => {
+const blogReducer = (state: BlogPost[], action: any): BlogPost[] => {
   switch (action.type) {
     case BlogPostActions.ADD_BLOG_POST:
-      return [...state, { title: `Another blog posts` }]
+      return [
+        ...state,
+        { title: `Another blog posts`, id: Math.floor(Math.random() * 9999) }
+      ]
+    case BlogPostActions.DELETE_BLOG_POST:
+      return state.filter((blogPost: BlogPost) => blogPost.id !== action.payload)
     default:
       return state
   }
 }
 
-const addBlogPost = dispatch => {
+type DispatchValue<T = any> = { type: BlogPostActions; payload?: T }
+
+const addBlogPost = (dispatch: Dispatch<DispatchValue>) => {
   return () => {
-    dispatch({type: BlogPostActions.ADD_BLOG_POST})
+    dispatch({ type: BlogPostActions.ADD_BLOG_POST })
+  }
+}
+
+const deleteBlogPost = (dispatch: Dispatch<DispatchValue<number>>) => {
+  return (id: number) => {
+    dispatch({ type: BlogPostActions.DELETE_BLOG_POST, payload: id })
   }
 }
 
 export const { Context, Provider } = createDataContext<BlogContextInterface>(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   [],
-  "blogPosts"
+  'blogPosts'
 )
--- src/screens/IndexScreen.tsx
+++ src/screens/IndexScreen.tsx
@@ -1,25 +1,57 @@
+import { Feather } from '@expo/vector-icons'
 import React, { useContext } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 import { Context } from '../context/BlogContext'
-
 const IndexScreen = () => {
-  const { blogPosts, addBlogPost } = useContext(Context)
+  const { blogPosts, addBlogPost, deleteBlogPost } = useContext(Context)
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button title='Add post' onPress={addBlogPost} />
       {!!blogPosts.length && (
         <FlatList
           data={blogPosts}
-          renderItem={({ item }) => <Text> {item.title} </Text>}
-          keyExtractor={blogPost => blogPost.title}
+          renderItem={({ item, index }) => {
+            const isLast = blogPosts.length === index + 1
+            return (
+              <View style={[styles.row, isLast ? styles.lastRow : null]}>
+                <Text style={styles.title}> {item.title} </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather name='trash-2' style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            )
+          }}
+          keyExtractor={blogPost => String(blogPost.id)}
         />
       )}
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'grey',
+    color: 'red'
+  },
+  lastRow: {
+    borderBottomWidth: 1
+  },
+  title: {
+    fontSize: 18,
+    textAlign:'center',
+  },
+  icon: {
+    paddingLeft: 15,
+    fontSize: 24,
+    marginRight: 15
+  }
+})
 
 export default IndexScreen
